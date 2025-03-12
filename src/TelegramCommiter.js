b68fec63e44d5c2ad304307a05df7c2e2b631918
VolotDmitriy <volotkovskiy.dmitriy@gmail.com>
2025-03-12 20:25:57 +0200

    fix bug with dotenv

--- src/TelegramCommiter.js
+++ src/TelegramCommiter.js
@@ -2,10 +2,16 @@ const express = require("express");
 const axios = require("axios");
 const logger = require("./utils/logger");
 
+require('dotenv').config();
+
 const app = express();
 app.use(express.json());
 
 app.use(logger);
+
+app.get("/", async (req, res) => {
+    res.send("Server is ready");
+})
 
 app.post("/", async (req, res) => {
     const commit = req.body.head_commit;
